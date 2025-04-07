09a0e42578b894de19db7315cda09eb54717be7c
Nick Kudriashov <nkdrov@gmail.com>
2025-04-06 22:15:19 -0400

    Delete supabase directory

--- supabase/migrations/20250303_add_location_coordinates.sql
+++
@@ -1,3 +0,0 @@
--- Add latitude and longitude columns to posts table
-ALTER TABLE posts ADD COLUMN IF NOT EXISTS latitude DOUBLE PRECISION;
-ALTER TABLE posts ADD COLUMN IF NOT EXISTS longitude DOUBLE PRECISION;
